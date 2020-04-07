badc7ad9ce3c64b78d3e4769f44f98fc8cbff20f
BerhKuhl <Berh.kuhl@gmail.com>
2020-04-07 17:43:54 -0300

    Aula 122 - Upcasting e downcasting (Reutilizado o programa da aula 121)

--- Aula 121 Herança/src/application/Program.java
+++ Aula 121 Herança/src/application/Program.java
@@ -1,10 +1,41 @@
 package application;
 
+import java.util.Locale;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Account acc = new Account(2403, "Bernardo", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1301, "Kauany", 0.0, 500.0);
 		
+		// UPCASTING
+		
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1324, "Pedro", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(4004, "Anna", 0.0, 0.01);
+		
+		// DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		// BusinessAccount acc5 = (BusinessAccount)acc3;
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(100.0);
+			System.out.println("Loan");
+		}
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 	}
 
 }
