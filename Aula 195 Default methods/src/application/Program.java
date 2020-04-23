9537052dbc58906cb97de1a22f057e194cbb218a
BerhKuhl <Berh.kuhl@gmail.com>
2020-04-23 16:14:59 -0300

    Aula 195 - Default methods

---
+++ Aula 195 Default methods/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import services.BrazilInterestService;
+import services.InterestService;
+import services.UsaInterestService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Amount: ");
+		double amount = sc.nextDouble();
+		System.out.print("Months: ");
+		int months = sc.nextInt();
+		
+		InterestService bis = new BrazilInterestService(2.0);
+		double payment = bis.payment(amount, months);
+		
+		System.out.println();
+		System.out.println("BRAZIL INTEREST SERVICE");
+		System.out.println("Payment after " + months + " months: ");
+		System.out.println(String.format("%.2f", payment));
+		
+		InterestService uis = new UsaInterestService(1.0);
+		payment = uis.payment(amount, months);
+		
+		System.out.println();
+		System.out.println("USA INTEREST SERVICE");
+		System.out.println("Payment after " + months + " months: ");
+		System.out.println(String.format("%.2f", payment));
+		
+		sc.close();
+	}
+
+}
