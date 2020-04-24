cb2d2f204797d4b9111de3a2569765790d3c29be
BerhKuhl <Berh.kuhl@gmail.com>
2020-04-24 16:04:42 -0300

    Aula 202 - HashCode e Equals

---
+++ Aula 202 HashCode e Equals/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String a = "2";
+		String b = "1";
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+	}
+
+}
