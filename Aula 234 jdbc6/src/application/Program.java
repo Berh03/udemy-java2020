e1baac8d02356cec00595c9d334ec962a41d579a
BerhKuhl <Berh.kuhl@gmail.com>
2020-06-10 12:30:35 -0300

    Aula 234 - Demo - Transações dados

---
+++ Aula 234 jdbc6/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false);
+			
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3092 WHERE DepartmentId = 2");
+			
+			System.out.println("Rows 1: "+rows1);
+			
+			/*int x = 1;
+			if (x < 2) {
+				throw new SQLException("Fake error");
+			}*/
+			
+			conn.commit();
+
+			System.out.println("Rows 2: "+rows2);
+			
+			
+		}catch (SQLException e) {
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Caused by: "+e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Trying rollback! Caused by: " +e1.getMessage());
+			}
+		}finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
